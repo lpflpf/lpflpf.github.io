eaeea4d344a20c8a35564b7d2799b7f46fdbf0be
lpflpf <lipengfei634626165@163.com>
2023-01-29 17:14:24 +0800

    修改themes

---
+++ assets/js/custom.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?fbf0a50f2173f7a03a2b6d7d535ef1d9";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
